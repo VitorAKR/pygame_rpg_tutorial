5ff9bee86125dd7440054ea86f8553b94859a527
VitorAKR <vitor.akr@gmail.com>
2025-05-24 13:54:40 -0300

    Created basic movement to our player

--- player.py
+++ player.py
@@ -6,3 +6,38 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load('./graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+
+		#use a vector to set moves to our player based on keys entries and multiplies with some of speed
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
+
+    def input(self):
+        #get all the keys pressed
+        keys = pygame.key.get_pressed()
+
+        #setting Y direction
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        #setting X direction
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def move(self,speed):
+        #normalizing the speed applied in 2 directions (trigonometry), by changing the lenght of a vector(magnitude) to 1
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+
+        self.rect.center += self.direction * speed
+
+    def update(self):
+        self.input()
+        self.move(self.speed)
