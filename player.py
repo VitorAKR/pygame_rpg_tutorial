d6d99c6e568991a2fa74e5a5d4f12d0b8a90fa46
VitorAKR <vitor.akr@gmail.com>
2025-06-07 09:50:15 -0300

    Overlaps by creating hitbox for tiles and player

--- player.py
+++ player.py
@@ -6,6 +6,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load('./graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+        self.hitbox = self.rect.inflate(0,-26)
 
 		#use a vector to set moves to our player based on keys entries and multiplies with some of speed
         self.direction = pygame.math.Vector2()
@@ -38,13 +39,14 @@ class Player(pygame.sprite.Sprite):
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
 
-        self.rect.x += self.direction.x * speed
+        self.hitbox.x += self.direction.x * speed
         #check for horizontal collisions
         self.collision('horizontal')
-        self.rect.y += self.direction.y * speed
+        self.hitbox.y += self.direction.y * speed
         #check for vertical collisions
         self.collision('vertical')
-		#self.rect.center += self.direction * speed
+        #changing collisions for hitbox, but rectangle must follow the hitbox
+        self.rect.center = self.hitbox.center
 
     #Collisions detectition:
     #1.Apply horizontal movement
@@ -54,21 +56,21 @@ class Player(pygame.sprite.Sprite):
     def collision(self, direction):
         if direction == 'horizontal':
             for sprite in self.obstacle_sprites:
-                #Checking the rectangle of the sprite with the rectangle of the player 
-                if sprite.rect.colliderect(self.rect):
+                #Checking the hitbox of the sprite with the hitbox of the player 
+                if sprite.hitbox.colliderect(self.hitbox):
                     if self.direction.x > 0: #player is moving to right
-                        self.rect.right = sprite.rect.left
+                        self.hitbox.right = sprite.hitbox.left
                     if self.direction.x < 0: #player is moving to left
-                        self.rect.left = sprite.rect.right
+                        self.hitbox.left = sprite.hitbox.right
 
         if direction == 'vertical':
             for sprite in self.obstacle_sprites:
-                #Checking the rectangle of the sprite with the rectangle of the player 
-                if sprite.rect.colliderect(self.rect):
+                #Checking the hitbox of the sprite with the hitbox of the player 
+                if sprite.hitbox.colliderect(self.hitbox):
                     if self.direction.y > 0: #player is moving down
-                        self.rect.bottom = sprite.rect.top
+                        self.hitbox.bottom = sprite.hitbox.top
                     if self.direction.y < 0: #player is moving up
-                        self.rect.top = sprite.rect.bottom
+                        self.hitbox.top = sprite.hitbox.bottom
 
     def update(self):
         self.input()
--- tile.py
+++ tile.py
@@ -6,3 +6,5 @@ class Tile(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load('./graphics/test/rock.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+        #creating a short box inside the rectangle that has -5px on top and bottom, but preserves center position
+        self.hitbox = self.rect.inflate(0,-10)
